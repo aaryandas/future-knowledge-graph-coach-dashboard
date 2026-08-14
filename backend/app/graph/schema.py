14e65aeca250468edacfc9a0bc6210ec4a9babfe
Aaryan Das <aaryan.das@challenger.gauntletai.com>
2026-08-13 20:42:51 -0500

    Implement KG2 member context ingest
    Ticket: GNT-239

--- backend/app/graph/schema.py
+++ backend/app/graph/schema.py
@@ -8,6 +8,14 @@ type NodeLabel = Literal[
     "Equipment",
     "AnatomicalStructure",
     "ClinicalFinding",
+    "Member",
+    "Goal",
+    "MemberInjury",
+    "WorkoutSession",
+    "Observation",
+    "ChatMessage",
+    "Barrier",
+    "CoachTask",
 ]
 type EdgeType = Literal[
     "targets",
@@ -17,10 +25,21 @@ type EdgeType = Literal[
     "findingSite",
     "isA",
     "exactMatch",
+    "pursues",
+    "has",
+    "owns",
+    "performed",
+    "observed",
+    "said",
+    "received",
+    "dislikes",
+    "included",
+    "evidencedBy",
+    "addresses",
 ]
 type TaxonomyField = tuple[str, NodeLabel, EdgeType, str]
 
-NODE_LABELS: tuple[NodeLabel, ...] = (
+KG1_NODE_LABELS: tuple[NodeLabel, ...] = (
     "Exercise",
     "MuscleGroup",
     "Joint",
@@ -30,7 +49,20 @@ NODE_LABELS: tuple[NodeLabel, ...] = (
     "ClinicalFinding",
 )
 
-EDGE_TYPES: tuple[EdgeType, ...] = (
+KG2_NODE_LABELS: tuple[NodeLabel, ...] = (
+    "Member",
+    "Goal",
+    "MemberInjury",
+    "WorkoutSession",
+    "Observation",
+    "ChatMessage",
+    "Barrier",
+    "CoachTask",
+)
+
+NODE_LABELS: tuple[NodeLabel, ...] = (*KG1_NODE_LABELS, *KG2_NODE_LABELS)
+
+KG1_EDGE_TYPES: tuple[EdgeType, ...] = (
     "targets",
     "loads",
     "performs",
@@ -38,6 +70,25 @@ EDGE_TYPES: tuple[EdgeType, ...] = (
     "findingSite",
     "isA",
     "exactMatch",
+)
+
+KG2_EDGE_TYPES: tuple[EdgeType, ...] = (
+    "pursues",
+    "has",
+    "owns",
+    "performed",
+    "observed",
+    "said",
+    "received",
+    "dislikes",
+    "included",
+    "exactMatch",
+    "evidencedBy",
+    "addresses",
+)
+
+EDGE_TYPES: tuple[EdgeType, ...] = tuple(
+    dict.fromkeys((*KG1_EDGE_TYPES, *KG2_EDGE_TYPES))
 )
 
 EXERCISE_TAXONOMIES: tuple[TaxonomyField, ...] = (
